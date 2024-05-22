1afc3a9979038c7744d9e266b369a964bb3488af
unknown <jperdomo@iegabo.edu.co>
2024-05-21 22:43:37 -0500

    tarea uno

--- ecommerce-jullopez08/src/middlewares/logger.middleware.ts
+++ ecommerce-jullopez08/src/middlewares/logger.middleware.ts
@@ -2,6 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
+//middleware local
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const date = new Date().toLocaleDateString();
@@ -12,13 +13,14 @@ export class LoggerMiddleware implements NestMiddleware {
     next();
   }
 }
+// middleware global
 export function LoggerGlobal(req: Request, res: Response, next: NextFunction) {
   const actualDate = new Date();
   const date = actualDate.toLocaleDateString();
   const time = actualDate.toLocaleTimeString();
 
   //get     // /users    //fecha   //hora
-  console.log(`${req.method} ${req.url} - ${date} ${time} loggeado`);
+  console.log(`${req.method} ${req.url} - ${date} ${time} global middleware`);
 
   next();
 }
